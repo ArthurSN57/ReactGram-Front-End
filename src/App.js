6eb3bc4a7552be4874f7ec3c0daffc5859e6c12a
ArthurSN57 <arthur.santos.nasc@outlook.com>
2024-04-16 18:06:45 -0300

    Started the redux use.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+import Home from './pages/Home/Home.js';
+import Login from './pages/Auth/Login.js';
+import Register from './pages/Auth/Register.js';
+
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
